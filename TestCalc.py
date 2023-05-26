17faf5e0cc7e257e95d3d429e042ee4f03dd6114
XxX_Nagibator228_XxX <61377502+DiVik-htelter@users.noreply.github.com>
2023-05-26 14:12:32 +0600

    Add files via upload

--- TestCalc.py
+++ TestCalc.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from calc_unit import credit
 #Test cases to test Calulator methods
 #You always create  a child class derived from unittest.TestCase
@@ -11,5 +12,12 @@ class TestCalc(unittest.TestCase):
 
   def test_deff(self):
     self.assertEqual(self.credit.deff(1000000,5,25), 1627083.3)
+
+  def test_url(self):
+    page = requests.get('http://127.0.0.1:5000')
+    self.assertEqual(page.status_code,200)
+#https://ya.ru/
+
+
 if __name__ == "__main__":
   unittest.main()
